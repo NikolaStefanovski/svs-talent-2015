8b8b09a9baf50c96a3c094f62c9029e26d488115
NikolaStefanovski <nklstefanovski@yahoo.com>
2015-04-22 17:02:39 +0200

    MVC Test app

---
+++ MVC_TestApp/MVC_TestApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using MVC_TestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Car car = new Car(6);
+            return View(car);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_TestApp/MVC_TestApp/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_TestApp.Models
+{
+    public class Car
+    {
+        private int _number;
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        public Car(int number)
+        {
+            _number = number;
+        }
+    }
+}
