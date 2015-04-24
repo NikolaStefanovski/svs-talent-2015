fdea51f2a0d305e73f8524492916556f29f7f6b6
NikolaStefanovski <nklstefanovski@yahoo.com>
2015-04-24 16:45:52 +0200

    misc

---
+++ 04-SPA/Project/Registar/Registar.DataLayer/BikeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Registar.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registar.DataLayer
+{
+    public class BikeConfiguration : EntityTypeConfiguration<Bike>
+    {
+        public BikeConfiguration()
+        {
+            this.ToTable("Bikes", "Data").HasKey(p => p.BikeId);
+
+            this.Property(p => p.BikeId).HasColumnName("Id");
+        }
+    }
+}
